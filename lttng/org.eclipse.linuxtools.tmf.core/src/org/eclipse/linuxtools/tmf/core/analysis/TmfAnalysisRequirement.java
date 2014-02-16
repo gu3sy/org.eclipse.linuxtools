8b993b720f023ca381129e835bbb66ce45fd258a
gu3sy <gu3sy@hotmail.com>
2014-02-15 20:31:01 -0500

    tmf: Implementation without the documentation

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/analysis/TmfAnalysisRequirement.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/analysis/TmfAnalysisRequirement.java
@@ -1,5 +1,6 @@
 package org.eclipse.linuxtools.tmf.core.analysis;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -12,12 +13,19 @@ public class TmfAnalysisRequirement {
     private final String fType;
     private final Map<String, TmfValueLevel> fValues = new HashMap<>();
 
+    /**
+     * @author
+     *
+     */
     public enum TmfValueLevel {
         MANDATORY,
         OPTIONAL,
         INFORMATIVE
     }
 
+    /**
+     * @param type
+     */
     public TmfAnalysisRequirement(String type) {
         fType = type;
     }
@@ -28,7 +36,10 @@ public class TmfAnalysisRequirement {
      */
     public TmfAnalysisRequirement(String type, Iterable<String> values, TmfValueLevel level) {
         fType = type;
-        fValues.clear();
+
+		for(String value : values) {
+			fValues.put(value, level);
+		}
     }
 
     /**
@@ -36,12 +47,38 @@ public class TmfAnalysisRequirement {
      * @param level
      */
     public void merge(TmfAnalysisRequirement req, TmfValueLevel level) {
+		/* If a value is already in a requirement, we update the level by the highest option (MANDATORY > OPTIONAL > INFORMATIVE) */
+		List<String> values = req.getValues();
+		for(String value: values) {
+			if(fValues.containsKey(value)) {
+				TmfValueLevel currentLevel = getValueLevel(value);
+				TmfValueLevel highestLevel = (currentLevel.ordinal() < level.ordinal()) ? currentLevel: level;
+				modifyValueLevel(value, highestLevel);
+			}
+			else {
+				addValue(value, level);
+			}
+		}
     }
 
     /**
      * @param req
      */
     public void merge(TmfAnalysisRequirement req) {
+		/* If a value is already in a requirement, we update the level by the highest option (MANDATORY > OPTIONAL > INFORMATIVE) */
+		List<String> values = req.getValues();
+		for(String value: values) {
+			TmfValueLevel reqLevel = req.getValueLevel(value);
+
+			if(fValues.containsKey(value)) {
+				TmfValueLevel currentLevel = getValueLevel(value);
+				TmfValueLevel highestLevel = (currentLevel.ordinal() < reqLevel.ordinal()) ? currentLevel: reqLevel;
+				modifyValueLevel(value, highestLevel);
+			}
+			else {
+				addValue(value, reqLevel);
+			}
+		}
     }
 
     /**
@@ -49,34 +86,59 @@ public class TmfAnalysisRequirement {
      * @param level
      */
     public void addValues(Iterable<String> values, TmfValueLevel level) {
+		for(String value : values) {
+			//TODO: do we test if the adding was successful?
+			addValue(value, level);
+		}
     }
 
     /**
      * @param value
      * @param level
+     * @return
      */
-    public void addValue(String value, TmfValueLevel level) {
+    public boolean addValue(String value, TmfValueLevel level) {
+		if(!fValues.containsKey(value)) {
+			fValues.put(value, level);
+			return true;
+		}
+		return false;
     }
 
     /**
      * @param value
      * @param level
+     * @return
      */
-    public void modifyValueLevel(String value, TmfValueLevel level) {
+    public boolean modifyValueLevel(String value, TmfValueLevel level) {
+		//TODO: do we force an addValueLevel before a call to modify?
+		if(fValues.containsKey(value)) {
+			fValues.put(value, level);
+			return true;
+		}
+		//TODO: return boolean that tells if the modifcation was successful?
+		return false;
     }
 
+    /**
+     * @return
+     */
     public String getType() {
         return fType;
     }
 
+    /**
+     * @return
+     */
     public List<String> getValues() {
-        return null;
+        return new ArrayList<>(fValues.keySet());
     }
 
     /**
      * @param value
+     * @return
      */
     public TmfValueLevel getValueLevel(String value) {
-        return null;
+        return fValues.get(value);
     }
 }
